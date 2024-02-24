8c2c9988cf47f6ecd9e14099120cf4462f2957ad
William <willspri@gmail.com>
2024-02-24 13:04:42 +1300

    added

---
+++ Conditions.java
@@ -0,0 +1,10 @@
+public class Conditions {
+	public static void main(String[] args) {
+		int number = 20;
+		if (number >= 18) {
+			System.out.println("numer is below 20");
+		} else { 
+			System.out.println("number is above 20");
+		}
+	}
+}
